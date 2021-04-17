83ce6e63495aafabd47dc13389cd35a289c2b2be
ajburns651 <ajburns651@gmail.com>
2021-04-17 16:36:17 -0700

    inital commit

--- Autolink.sh
+++ Autolink.sh
@@ -2,5 +2,5 @@
 echo "Links to Different Notes" > index.md
 for i in $(git ls-tree -r main --name-only | grep html)
 do
-         echo "<a href=$i>title=$i>$i</a>" >> index.md
+         echo "<a href=$i>title=$i>$i</a>\\" >> index.md
 done
