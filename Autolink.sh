ab7b3ed0cd9eb35f33c903cbae0df8ba0da7d702
ajburns651 <ajburns651@gmail.com>
2021-04-17 17:49:59 -0700

    inital commit

--- Autolink.sh
+++ Autolink.sh
@@ -5,5 +5,5 @@ printf "%b\n" $(git ls-tree -r main --name-only | grep html) > files.txt
 sed -e 's/^/<a href="/' files.txt > filesnew.txt
 sed -i -e 's|$|">title=|' filesnew.txt
 echo "$(cat files.txt)" | sed 's:.*/::' > filesnobefore.txt
-echo $(paste -d'\0' filesnew.txt files.txt) >> filesnewest.txt
-sed -i -e 's|$|></a>|' filesnewest.txt
+paste -d'\0' filesnew.txt filesnobefore.txt > filesnewest.txt
+sed -e 's|$|></a>|' filesnewest.txt >> index.md
--- index.md
+++ index.md
@@ -1,2 +1,5 @@
 Links to Different Notes
 
+<a href="English/Essay/RA/essaynotes.html">title=essaynotes.html></a>
+<a href="Physics/Review.html">title=Review.html></a>
+<a href="Physics/Review1.html">title=Review1.html></a>
