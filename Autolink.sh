798ea41cc847dbb545c45097ffab84d3cfefa87f
ajburns651 <ajburns651@gmail.com>
2021-04-17 18:20:45 -0700

    inital commit

--- Autolink.sh
+++ Autolink.sh
@@ -3,10 +3,10 @@ echo "Links to Different Notes" > index.md
 echo "" >> index.md
 printf "%b\n" $(git ls-tree -r main --name-only | grep html) > files.txt
 sed -e 's/^/<a href="/' files.txt > filesnew.txt
-sed -i -e 's|$|">title=|' filesnew.txt
+sed -i -e 's|$|">|' filesnew.txt
 echo "$(cat files.txt)" | sed 's:.*/::' > filesnobefore.txt
 sed -i -e 's/^/"/' filesnobefore.txt
 sed -i -e 's/\.[^.]*$//' filesnobefore.txt
 sed -i -e 's/$/"/' filesnobefore.txt
 paste -d'\0' filesnew.txt filesnobefore.txt > filesnewest.txt
-sed -e 's|$|>A</a>|' filesnewest.txt >> index.md
+sed -e 's|$|</a>|' filesnewest.txt >> index.md
--- index.md
+++ index.md
@@ -1,5 +1,5 @@
 Links to Different Notes
 
-<a href="English/Essay/RA/essaynotes.html">title="essaynotes">A</a>
-<a href="Physics/Review.html">title="Review">A</a>
-<a href="Physics/Review1.html">title="Review1">A</a>
+<a href="English/Essay/RA/essaynotes.html">"essaynotes"</a>
+<a href="Physics/Review.html">"Review"</a>
+<a href="Physics/Review1.html">"Review1"</a>
