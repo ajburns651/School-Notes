ad4c77c89917df08dc79a715f0d4d397152ddc8d
ajburns651 <ajburns651@gmail.com>
2021-04-17 18:39:44 -0700

    inital commit

--- Autolink.sh
+++ Autolink.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "Links to Different Notes" > index.md
 echo "" >> index.md
 
 # Physics Section
@@ -19,6 +18,58 @@ echo "" >> index.md
 echo "**English**" >> index.md
 echo "" >> index.md
 printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep English) > files.txt
+sed -e 's/^/<a href="/' files.txt > filesnew.txt
+sed -i -e 's|$|">|' filesnew.txt
+echo "$(cat files.txt)" | sed 's:.*/::' > filesnobefore.txt
+sed -i -e 's/\.[^.]*$//' filesnobefore.txt
+paste -d'\0' filesnew.txt filesnobefore.txt > filesnewest.txt
+sed -e 's|$|</a>|' filesnewest.txt >> index.md
+rm -rf files.txt filesnew.txt filesnobefore.txt filesnewest.txt
+echo "" >> index.md
+
+# History Section
+echo "**History**" >> index.md
+echo "" >> index.md
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep History) > files.txt
+sed -e 's/^/<a href="/' files.txt > filesnew.txt
+sed -i -e 's|$|">|' filesnew.txt
+echo "$(cat files.txt)" | sed 's:.*/::' > filesnobefore.txt
+sed -i -e 's/\.[^.]*$//' filesnobefore.txt
+paste -d'\0' filesnew.txt filesnobefore.txt > filesnewest.txt
+sed -e 's|$|</a>|' filesnewest.txt >> index.md
+rm -rf files.txt filesnew.txt filesnobefore.txt filesnewest.txt
+echo "" >> index.md
+
+# Marketing Section
+echo "**Marketing**" >> index.md
+echo "" >> index.md
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep Marketing) > files.txt
+sed -e 's/^/<a href="/' files.txt > filesnew.txt
+sed -i -e 's|$|">|' filesnew.txt
+echo "$(cat files.txt)" | sed 's:.*/::' > filesnobefore.txt
+sed -i -e 's/\.[^.]*$//' filesnobefore.txt
+paste -d'\0' filesnew.txt filesnobefore.txt > filesnewest.txt
+sed -e 's|$|</a>|' filesnewest.txt >> index.md
+rm -rf files.txt filesnew.txt filesnobefore.txt filesnewest.txt
+echo "" >> index.md
+
+# Math Section
+echo "**Math**" >> index.md
+echo "" >> index.md
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep Math) > files.txt
+sed -e 's/^/<a href="/' files.txt > filesnew.txt
+sed -i -e 's|$|">|' filesnew.txt
+echo "$(cat files.txt)" | sed 's:.*/::' > filesnobefore.txt
+sed -i -e 's/\.[^.]*$//' filesnobefore.txt
+paste -d'\0' filesnew.txt filesnobefore.txt > filesnewest.txt
+sed -e 's|$|</a>|' filesnewest.txt >> index.md
+rm -rf files.txt filesnew.txt filesnobefore.txt filesnewest.txt
+echo "" >> index.md
+
+# Video-Film Section
+echo "**Video-Film**" >> index.md
+echo "" >> index.md
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep Video-Film) > files.txt
 sed -e 's/^/<a href="/' files.txt > filesnew.txt
 sed -i -e 's|$|">|' filesnew.txt
 echo "$(cat files.txt)" | sed 's:.*/::' > filesnobefore.txt
--- index.md
+++ index.md
@@ -9,3 +9,29 @@ Links to Different Notes
 
 <a href="English/Essay/RA/essaynotes.html">essaynotes</a>
 
+
+**Physics**
+
+<a href="Physics/Review.html">Review</a>
+<a href="Physics/Review1.html">Review1</a>
+
+**English**
+
+<a href="English/Essay/RA/essaynotes.html">essaynotes</a>
+
+**History**
+
+<a href=""></a>
+
+**Marketing**
+
+<a href=""></a>
+
+**Math**
+
+<a href=""></a>
+
+**Video-Film**
+
+<a href=""></a>
+
