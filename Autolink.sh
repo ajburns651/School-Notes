6395189cc43fd42f1bcc359d8db06ac0438b3d6e
ajburns651 <ajburns651@gmail.com>
2021-04-17 17:05:07 -0700

    inital commit

--- Autolink.sh
+++ Autolink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Links to Different Notes" > index.md
 printf "%b\n" $(git ls-tree -r main --name-only | grep html) > files.txt
-sed -i -e 's/^/<a href="/' files.txt
-sed -i -e 's/$/">/' files.txt
-cat files.txt >> index.md
+sed -e 's/^/<a href="/' files.txt > filesnew.txt
+sed -i -e 's|$|">Wow</a>|' filesnew.txt
+cat filesnew.txt >> index.md
