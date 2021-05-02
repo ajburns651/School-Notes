f57e63fbec4cbdaab5cc2c5dfbf766051cb9ecb8
ajburns651 <ajburns651@gmail.com>
2021-05-02 14:41:09 -0700

    New  Notes

---
+++ English/Essay/Synthesis/.auctex-auto/notes.el
@@ -0,0 +1,44 @@
+(TeX-add-style-hook
+ "notes"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org1b0d663"
+    "sec:org5290443"
+    "sec:org614fd97"
+    "sec:org3d44d02"
+    "sec:orgc383be8"
+    "sec:org828b144"
+    "sec:orgd5dc18b"
+    "sec:org2ac355e"
+    "sec:orga65759d"))
+ :latex)
+
