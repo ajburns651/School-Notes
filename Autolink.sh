e54eb01f2ee4fd193f4e277d2f883d180cf4f374
ajburns651 <ajburns651@gmail.com>
2021-08-19 22:07:56 -0700

    New  Notes

--- Autolink.sh
+++ Autolink.sh
@@ -59,16 +59,7 @@ echo "" >> index.md
 # Calc3 Section
 echo "**Calc3**" >> index.md
 echo "" >> index.md
-printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep Calc3) > files.txt
-sed -e 's/^/<a href="/' files.txt > filesnew.txt
-sed -i -e 's|$|">|' filesnew.txt
-echo "$(cat files.txt)" | sed -et -e's|/|&\n|1;/\n/D' | tr / " "  > filesnobefore.txt
-sed -i -e 's/\.[^.]*$//' filesnobefore.txt
-paste -d'\0' filesnew.txt filesnobefore.txt > filesnewest.txt
-sed -i -e 's|$|</a>|' filesnewest.txt
-sed -e '0~1 a\\' filesnewest.txt >> index.md
-rm -rf files.txt filesnew.txt filesnobefore.txt filesnewest.txt
-echo "" >> index.md
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep
 
 # Gov Section
 echo "**Gov**" >> index.md
