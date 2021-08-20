937a92a313c17b57e32008cbb8bac9094c738de5
ajburns651 <ajburns651@gmail.com>
2021-08-19 21:51:26 -0700

    resetup

--- Autolink.sh
+++ Autolink.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "" > index.md
 
-# Physics Section
-echo "**Physics**" >> index.md
+# AdvBusMgmt Section
+echo "**AdvBusMgmt**" >> index.md
 echo "" >> index.md
-printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep Physics) > files.txt
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep AdvBusMgmt) > files.txt
 sed -e 's/^/<a href="/' files.txt > filesnew.txt
 sed -i -e 's|$|">|' filesnew.txt
 echo "$(cat files.txt)" | sed -et -e's|/|&\n|1;/\n/D' | tr / " "  > filesnobefore.txt
@@ -15,10 +15,10 @@ sed -e '0~1 a\\' filesnewest.txt >> index.md
 rm -rf files.txt filesnew.txt filesnobefore.txt filesnewest.txt
 echo "" >> index.md
 
-# English Section
-echo "**English**" >> index.md
+# APCompSciPrinc Section
+echo "**APCompSciPrinc**" >> index.md
 echo "" >> index.md
-printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep English) > files.txt
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep APCompSciPrinc) > files.txt
 sed -e 's/^/<a href="/' files.txt > filesnew.txt
 sed -i -e 's|$|">|' filesnew.txt
 echo "$(cat files.txt)" | sed -et -e's|/|&\n|1;/\n/D' | tr / " "  > filesnobefore.txt
@@ -28,24 +28,10 @@ sed -i -e 's|$|</a>|' filesnewest.txt
 sed -e '0~1 a\\' filesnewest.txt >> index.md
 echo "" >> index.md
 
-# History Section
-echo "**History**" >> index.md
+# APLit Section
+echo "**APLit**" >> index.md
 echo "" >> index.md
-printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep History) > files.txt
-sed -e 's/^/<a href="/' files.txt > filesnew.txt
-sed -i -e 's|$|">|' filesnew.txt
-echo "$(cat files.txt)" | sed -et -e's|/|&\n|1;/\n/D' | tr / " "  > filesnobefore.txt
-sed -i -e 's/\.[^.]*$//' filesnobefore.txt
-paste -d'\0' filesnew.txt filesnobefore.txt > filesnewest.txt
-sed -i -e 's|$|</a>|' filesnewest.txt
-sed -e '0~1 a\\' filesnewest.txt >> index.md
-rm -rf files.txt filesnew.txt filesnobefore.txt filesnewest.txt
-echo "" >> index.md
-
-# Marketing Section
-echo "**Marketing**" >> index.md
-echo "" >> index.md
-printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep Marketing) > files.txt
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep APLit) > files.txt
 sed -e 's/^/<a href="/' files.txt > filesnew.txt
 sed -i -e 's|$|">|' filesnew.txt
 echo "$(cat files.txt)" | sed -et -e's|/|&\n|1;/\n/D' | tr / " "  > filesnobefore.txt
@@ -56,10 +42,10 @@ sed -e '0~1 a\\' filesnewest.txt >> index.md
 rm -rf files.txt filesnew.txt filesnobefore.txt filesnewest.txt
 echo "" >> index.md
 
-# Math Section
-echo "**Math**" >> index.md
+# APStats Section
+echo "**APStats**" >> index.md
 echo "" >> index.md
-printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep Math) > files.txt
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep APStats) > files.txt
 sed -e 's/^/<a href="/' files.txt > filesnew.txt
 sed -i -e 's|$|">|' filesnew.txt
 echo "$(cat files.txt)" | sed -et -e's|/|&\n|1;/\n/D' | tr / " "  > filesnobefore.txt
@@ -70,10 +56,24 @@ sed -e '0~1 a\\' filesnewest.txt >> index.md
 rm -rf files.txt filesnew.txt filesnobefore.txt filesnewest.txt
 echo "" >> index.md
 
-# Video-Film Section
-echo "**Video-Film**" >> index.md
+# Calc3 Section
+echo "**Calc3**" >> index.md
 echo "" >> index.md
-printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep Video-Film) > files.txt
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep Calc3) > files.txt
+sed -e 's/^/<a href="/' files.txt > filesnew.txt
+sed -i -e 's|$|">|' filesnew.txt
+echo "$(cat files.txt)" | sed -et -e's|/|&\n|1;/\n/D' | tr / " "  > filesnobefore.txt
+sed -i -e 's/\.[^.]*$//' filesnobefore.txt
+paste -d'\0' filesnew.txt filesnobefore.txt > filesnewest.txt
+sed -i -e 's|$|</a>|' filesnewest.txt
+sed -e '0~1 a\\' filesnewest.txt >> index.md
+rm -rf files.txt filesnew.txt filesnobefore.txt filesnewest.txt
+echo "" >> index.md
+
+# Gov Section
+echo "**Gov**" >> index.md
+echo "" >> index.md
+printf "%b\n" $(git ls-tree -r main --name-only | grep html | grep Gov) > files.txt
 sed -e 's/^/<a href="/' files.txt > filesnew.txt
 sed -i -e 's|$|">|' filesnew.txt
 echo "$(cat files.txt)" | sed -et -e's|/|&\n|1;/\n/D' | tr / " "  > filesnobefore.txt
