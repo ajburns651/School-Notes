c62e1f4a72fd132cdad6785e7702b60604b60b24
ajburns651 <ajburns651@gmail.com>
2021-11-09 17:59:33 -0800

    New  Notes

--- link.sh
+++ link.sh
@@ -2,8 +2,17 @@
 
 cd /home/keb/Documents/School-Org-Notes/content/School/
 
-#Advanced Business Management Section
-printf "%b\n" "$(git ls-tree -r main --name-only | grep org | grep AdvBusMgmt)" > list.txt
+# Define classes as a list of variables
+class=(AdvBusMgmt APCompSciPrinc Calc3 APStats Gov APLit)
+homepage=(AdvancedBusinessManagement.org APComputerScience.org Calculus3.org APStatistics.org History.org Literature.org)
+
+# Create Links to Note Files for Homepages
+for ((i = 0; i < ${#class[@]}; i++)); do
+
+echo "${class[$i]}"
+echo "${homepage[$i]}"
+
+printf "%b\n" $(git ls-tree -r main --name-only | grep org | grep ${class[$i]}) > list.txt
 sed 's/^/\//' list.txt > withslash.txt
 sed 's/^/- [[./' withslash.txt > beginningdone.txt
 sed -i 's/$/][&/' beginningdone.txt #Adds end of link brackets and adds beginning of description
@@ -11,77 +20,9 @@ echo "$(cat list.txt)" | sed 's/^.*[/]//' > filenameonly.txt
 sed -i -e 's/\.[^.]*$//' filenameonly.txt #removes.org extension
 paste -d'\0' beginningdone.txt filenameonly.txt > withoutending.txt #Pastes the names after the start of the description from line 7
 sed 's/$/]]&/' withoutending.txt > final.txt #Adds the end of the link
-sed -i '/* Notes/,+100 d' ./AdvancedBusinessManagement.org
-echo "* Notes" >> ./AdvancedBusinessManagement.org
-cat final.txt >> ./AdvancedBusinessManagement.org #Pastes the names after the start of the description from line 7
+sed -i '/* Notes/,+100 d' ./"${homepage[$i]}"
+echo "* Notes" >> ./"${homepage[$i]}"
+cat final.txt >> ./"${homepage[$i]}" #Pastes the names after the start of the description from line 7
 rm -rf list.txt withslash.txt beginningdone.txt filenameonly.txt withoutending.txt final.txt
 
-#APComputer Science Section
-printf "%b\n" "$(git ls-tree -r main --name-only | grep org | grep APCompSciPrinc)" > list.txt
-sed 's/^/\//' list.txt > withslash.txt
-sed 's/^/- [[./' withslash.txt > beginningdone.txt
-sed -i 's/$/][&/' beginningdone.txt #Adds end of link brackets and adds beginning of description
-echo "$(cat list.txt)" | sed 's/^.*[/]//' > filenameonly.txt
-sed -i -e 's/\.[^.]*$//' filenameonly.txt #removes.org extension
-paste -d'\0' beginningdone.txt filenameonly.txt > withoutending.txt #Pastes the names after the start of the description from line 7
-sed 's/$/]]&/' withoutending.txt > final.txt #Adds the end of the link
-sed -i '/* Notes/,+100 d' ./APComputerScience.org
-echo "* Notes" >> ./APComputerScience.org
-cat final.txt >> ./APComputerScience.org #Pastes the names after the start of the description from line 7
-rm -rf list.txt withslash.txt beginningdone.txt filenameonly.txt withoutending.txt final.txt
-
-#Calculus 3 Section
-printf "%b\n" "$(git ls-tree -r main --name-only | grep org | grep Calc3)" > list.txt
-sed 's/^/\//' list.txt > withslash.txt
-sed 's/^/- [[./' withslash.txt > beginningdone.txt
-sed -i 's/$/][&/' beginningdone.txt #Adds end of link brackets and adds beginning of description
-echo "$(cat list.txt)" | sed 's/^.*[/]//' > filenameonly.txt
-sed -i -e 's/\.[^.]*$//' filenameonly.txt #removes.org extension
-paste -d'\0' beginningdone.txt filenameonly.txt > withoutending.txt #Pastes the names after the start of the description from line 7
-sed 's/$/]]&/' withoutending.txt > final.txt #Adds the end of the link
-sed -i '/* Notes/,+100 d' ./Calculus3.org
-echo "* Notes" >> ./Calculus3.org
-cat final.txt >> ./Calculus3.org #Pastes the names after the start of the description from line 7
-rm -rf list.txt withslash.txt beginningdone.txt filenameonly.txt withoutending.txt final.txt
-
-#APStats Section
-printf "%b\n" "$(git ls-tree -r main --name-only | grep org | grep APStats)" > list.txt
-sed 's/^/\//' list.txt > withslash.txt
-sed 's/^/- [[./' withslash.txt > beginningdone.txt
-sed -i 's/$/][&/' beginningdone.txt #Adds end of link brackets and adds beginning of description
-echo "$(cat list.txt)" | sed 's/^.*[/]//' > filenameonly.txt
-sed -i -e 's/\.[^.]*$//' filenameonly.txt #removes.org extension
-paste -d'\0' beginningdone.txt filenameonly.txt > withoutending.txt #Pastes the names after the start of the description from line 7
-sed 's/$/]]&/' withoutending.txt > final.txt #Adds the end of the link
-sed -i '/* Notes/,+100 d' ./APStatistics.org
-echo "* Notes" >> ./APStatistics.org
-cat final.txt >> ./APStatistics.org #Pastes the names after the start of the description from line 7
-rm -rf list.txt withslash.txt beginningdone.txt filenameonly.txt withoutending.txt final.txt
-
-#Government/Econ Section
-printf "%b\n" "$(git ls-tree -r main --name-only | grep org | grep Gov)" > list.txt
-sed 's/^/\//' list.txt > withslash.txt
-sed 's/^/- [[./' withslash.txt > beginningdone.txt
-sed -i 's/$/][&/' beginningdone.txt #Adds end of link brackets and adds beginning of description
-echo "$(cat list.txt)" | sed 's/^.*[/]//' > filenameonly.txt
-sed -i -e 's/\.[^.]*$//' filenameonly.txt #removes.org extension
-paste -d'\0' beginningdone.txt filenameonly.txt > withoutending.txt #Pastes the names after the start of the description from line 7
-sed 's/$/]]&/' withoutending.txt > final.txt #Adds the end of the link
-sed -i '/* Notes/,+100 d' ./History.org
-echo "* Notes" >> ./History.org
-cat final.txt >> ./History.org #Pastes the names after the start of the description from line 7
-rm -rf list.txt withslash.txt beginningdone.txt filenameonly.txt withoutending.txt final.txt
-
-#AP Lit Section
-printf "%b\n" "$(git ls-tree -r main --name-only | grep org | grep APLit)" > list.txt
-sed 's/^/\//' list.txt > withslash.txt
-sed 's/^/- [[./' withslash.txt > beginningdone.txt
-sed -i 's/$/][&/' beginningdone.txt #Adds end of link brackets and adds beginning of description
-echo "$(cat list.txt)" | sed 's/^.*[/]//' > filenameonly.txt
-sed -i -e 's/\.[^.]*$//' filenameonly.txt #removes.org extension
-paste -d'\0' beginningdone.txt filenameonly.txt > withoutending.txt #Pastes the names after the start of the description from line 7
-sed 's/$/]]&/' withoutending.txt > final.txt #Adds the end of the link
-sed -i '/* Notes/,+100 d' ./Literature.org
-echo "* Notes" >> ./Literature.org
-cat final.txt >> ./Literature.org #Pastes the names after the start of the description from line 7
-rm -rf list.txt withslash.txt beginningdone.txt filenameonly.txt withoutending.txt final.txt
+done
